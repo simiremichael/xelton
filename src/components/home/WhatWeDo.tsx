d7bb0ae814176a2028daf4e61c3764c27cf8193d
Michael Simire <simiremichael@gmail.com>
2023-11-30 10:36:29 +0100

    updated

--- src/components/home/WhatWeDo.tsx
+++ src/components/home/WhatWeDo.tsx
@@ -3,6 +3,10 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import backImg from '../../assets/what-we-do.png'
 import { Grid } from '@mui/material';
+import vectorWeb from '../../assets/vector-web.svg'
+import vectorErp from '../../assets/vector-erp.svg'
+import vectorMobile from '../../assets/vector-mobile.svg'
+import vectorUx from '../../assets/vector-ux.svg'
 
 
 const StyledBox = styled(Box)`
@@ -11,6 +15,7 @@ background-position: center;
 background-repeat: no-repeat;
 background-size: cover;
 width: 100%;
+padding-bottom: 20px;
 `
 const StyledContainer = styled(Container)`
 height: auto;
@@ -61,6 +66,16 @@ align-items: center;
 gap: 25px;
           }
 `
+const CardIcon = styled.img`
+@media screen and (max-width: 600px) {
+width: 22px;
+height: 22px;
+}
+@media screen and (min-width: 600px) {
+    width: 52px;
+    height: 52px;
+    }
+`
 const CardHeader = styled.h4`
 color: #fff;
 font-family: Puritan;
@@ -69,6 +84,8 @@ line-height: normal;
 letter-spacing: -0.363px;
 font-size: 22px;
 font-weight: 700;
+margin-bottom: 0;
+margin-top: 0;
 @media screen and (max-width: 600px) {
 color: #fff;
 font-family: Puritan;
@@ -90,6 +107,7 @@ line-height: normal;
 letter-spacing: -0.297px;
 margin-right: 5%;
 margin-left: 5%;
+margin-top: 0;
 @media screen and (max-width: 600px) {
 color: rgba(217, 217, 217, 0.79);
 text-align: center;
@@ -112,24 +130,28 @@ function WhatWeDo() {
         <Grid container spacing={6} columnSpacing={8}>
          <Grid item lg={6} md={6} sm={6} xs={12}>
           <Card>
+            <CardIcon src={vectorErp} />
             <CardHeader>Custom Software Development</CardHeader>
          <CardContent>Tailored solutions designed to address the specific needs and challenges of your business, ensuring optimal performance and efficiency.</CardContent>
           </Card>
          </Grid>
          <Grid item lg={6} md={6} sm={6} xs={12}>
           <Card>
+          <CardIcon src={vectorMobile} />
             <CardHeader>Mobile App Development</CardHeader>
          <CardContent>Innovative mobile applications for IOS and Android platforms, combining user-centric design with seamless functionality to engage your target audience.</CardContent>
           </Card>
          </Grid>
          <Grid item lg={6} md={6} sm={6} xs={12}>
           <Card>
+          <CardIcon src={vectorWeb} />
             <CardHeader>Web Development</CardHeader>
          <CardContent>Robust and scalable web solutions, from dynamic websites to complex web applications, crafted to enhance your online presence and business growth.</CardContent>
           </Card>
          </Grid>
          <Grid item lg={6} md={6} sm={6} xs={12}>
           <Card>
+          <CardIcon src={vectorUx} />
             <CardHeader>UI/UX Design</CardHeader>
          <CardContent>Intuitive and visually appealing user interfaces, coupled with a focus on user experience, to ensure that your software not only flawlessly but is a pleasure to use.</CardContent>
           </Card>
