e3037d21916ce197378cfe8758c01fe192522ade
Michael Simire <simiremichael@gmail.com>
2023-12-04 15:33:39 +0100

    updated

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import logo from '../../assets/logo.svg'
 import { NavLink} from "react-router-dom";
+import { Menu, MenuItem } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import React from 'react';
 
 const StyledBox = styled(Box)`
 background: rgba(255, 255, 255, 0.2);
@@ -78,12 +81,58 @@ border-radius: 10px;
 border: 0.71px solid rgba(255, 0, 0, 0.20);
 background: #FFF;
 `
+const MobileHireUsBtn = styled.button`
+display: flex;
+padding: 10px;
+justify-content: center;
+align-items: center;
+gap: 10px;
+color: #F00;
+cursor: pointer;
+border-radius: 10px;
+background: #FFF;
+outline: none;
+border: none;
+`
 const StyledLink = styled(NavLink)`
 text-decoration: none;
+`
+const StyledLinks = styled(NavLink)`
+text-decoration: none;
+color: #050507;
+font-family: Puritan;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+letter-spacing: -0.264px;
+:active {
+    font-weight: bold;
+}
+`
+const BuggerMenuContainer = styled.div`
+@media screen and (min-width: 901px) {
+    display: none;
+  }
+`
+const StyledButton  = styled.button`
+border: none;
+outlined: none;
+@media screen and (min-width: 901px) {
+    display: none;
+  }
 `
 
 
 function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <StyledBox>
         <StyledContainer>
@@ -96,7 +145,31 @@ function NavBar() {
                 <StyledLink id='navbar' to='/about'><MenuItems>About</MenuItems></StyledLink>
                 <StyledLink id='navbar' to='/contact'><MenuItems>Contact</MenuItems></StyledLink>
                 <HireUsBtn>Hire Us</HireUsBtn>
+                <BuggerMenuContainer>
+                <StyledButton
+                  id="basic-button"
+                  aria-controls={open ? 'basic-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                  onClick={handleClick}
+                    >
+                  <MenuOutlinedIcon />
+                 </StyledButton>
+                </BuggerMenuContainer>
             </MenuContainer>
+            <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}> <StyledLinks id='navbar' to='/about'>About</StyledLinks></MenuItem>
+        <MenuItem onClick={handleClose}>  <StyledLinks id='navbar' to='/contact'>Contact</StyledLinks></MenuItem>
+        <MenuItem onClick={handleClose}> <MobileHireUsBtn>Hire Us</MobileHireUsBtn></MenuItem>
+      </Menu>
         </StyledContainer>
     </StyledBox>
   )
